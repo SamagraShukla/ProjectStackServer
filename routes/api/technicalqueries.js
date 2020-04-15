03ce3b46e6d189e873c9412fad57e479a89dcab1
Deepu006 <43022188+Deepu006@users.noreply.github.com>
2020-04-15 17:18:26 -0400

    Update technicalqueries.js

--- routes/api/technicalqueries.js
+++ routes/api/technicalqueries.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 
 let TechnicalQuery = require('../../models/Technicalqueries');
+const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
 //route Get api/technicalqueries
 //desc Get all technicalqueries
 //access public
-router.get('/', async (req,res) => {
+router.get('/', auth, async (req,res) => {
     try{
         const QueryDB = await TechnicalQuery.find();
         res.json(QueryDB);
@@ -21,9 +22,9 @@ router.get('/', async (req,res) => {
 //route Get api/technicalqueries/:id
 //desc Get all technicalqueries by id
 //access public 
-router.get('/:id', async (req,res) => {
+router.get('/:id', auth, async (req,res) => {
     try{
-        const query = await TechnicalQuery.findById(req.body.id);
+        const query = await TechnicalQuery.findById(req.params.id);
         if(!query){
             return res.status(404).send('TechnicalQuery not found');
         }
@@ -37,7 +38,7 @@ router.get('/:id', async (req,res) => {
 //route post api/technicalqueries
 //desc insert technicalquery
 //access public 
-router.post('/', [
+router.post('/', auth, [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required').not().isEmpty(),
     check('query', 'Query is required').not().isEmpty()
@@ -67,9 +68,9 @@ router.post('/', [
 //route update api/technicalqueries/:id
 //desc update all technicalqueries by id
 //access public 
-router.put('/update/:id', async (req,res) => {
+router.put('/update/:id', auth, async (req,res) => {
     
-    const queryUpdate = await TechnicalQuery.findById(req.body.id);
+    const queryUpdate = await TechnicalQuery.findById(req.params.id);
     
     queryUpdate.name = req.body.name;
     queryUpdate.email = req.body.email;
@@ -84,11 +85,11 @@ router.put('/update/:id', async (req,res) => {
 //route delete api/technicalqueries/:id
 //desc delete technicalquery by id
 //access public 
-router.delete('/:id', async (req,res) => {
+router.delete('/:id', auth, async (req,res) => {
     try {
         // find the element
-        await TechnicalQuery.findByIdAndRemove({_id:req.body.id});    
-        res.json({ msg: 'Query deleted' });
+        await TechnicalQuery.findByIdAndRemove({_id:req.params.id});    
+        res.json({ status:0 , msg: 'Query deleted' });
       } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
